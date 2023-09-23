ccb5192d762ec6bb8fe98884232bb5d76fcc67d1
andrei-ned <nedelcuandrei64@gmail.com>
2023-09-24 02:42:47 +0300

    cleaned up comments and debug code

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,9 +8,10 @@ using ChessChallenge.Application;
 #endif
 
 // TODO
-// - shrink size within 1024
-// test TTEntry size again
+// DONE - shrink size within 1024
+// DONE test TTEntry size again
 // play around with some values (e.g. piece values, attack bonuses)
+// can we fit anti extensions for moves that aren't first in the ordering?
 
 // TT:                       1130
 // Eval:                     1102
@@ -45,28 +46,11 @@ public class MyBot : IChessBot
     private int[] _pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
     private int[] _bonusPointsPerAttackEarly = { 0, 0, 4, 5, 1, 1, 0 };
     private int[] _bonusPointsPerAttackLate = { 0, 0, 2, 3, 5, 3, 1 };
-    //private TranspositionTable _transpositionTable = new TranspositionTable(32000);
-    //private Board board;
-    //private Move _bestMove;
-
-    //private bool IsLategame => _board.PlyCount > 30;
-
     private int[] _moveScores = new int[218]; // for sorting moves
-    //private Dictionary<Move, int> _moveScoresDict = new Dictionary<Move, int>();
-
-    //private int _currentDepth;
-
-    // Time management
-    //private Timer _timer;
-    //private int _maxMillis;
-    //private bool ShouldCancel => _timer.MillisecondsElapsedThisTurn > _maxMillis;
-
     Random _rng = new Random();
 
     // Transposition table
-    //private const int _ttCapacity = 32000;
     private TTEntry[] _ttEntries = new TTEntry[32000];
-    //private ulong _ttIndex => board.ZobristKey % 32000;
 
 #if DEBUG
     private int _evalCount;
@@ -76,73 +60,18 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        //this.board = board;
-        //_timer = timer;
-
         // Forced move, don't waste time searching
         Span<Move> legalMoves = stackalloc Move[218];
         board.GetLegalMovesNonAlloc(ref legalMoves);
         Move bestMove = legalMoves[0];
         if (legalMoves.Length == 1)
-#if DEBUG
-        {
-            ConsoleHelper.Log($"Forced {legalMoves[0]}", false, ConsoleColor.Magenta);
-#endif
             return bestMove;
-#if DEBUG
-        }
-#endif
-        
-#if DEBUG
-        //_moveScoresDict.Clear();
-        //ConsoleHelper.Log($"Entry size is {TranspositionTable.Entry.GetSize()} bytes", false);
-        _evalCount = 0;
-        _tableHitCount = 0;
-        //int bestEval =
-        int bestEval = 0;
-        _maxPly = 0;
-#endif
-
-        //_maxMillis = 100;
-        //int targetMillis = (Math.Min(1000, timer.MillisecondsRemaining / 40) + timer.IncrementMilliseconds) / 4;
 
         // Iterative deepening
         for (int currentDepth = 1; timer.MillisecondsElapsedThisTurn < (Math.Min(1000, timer.MillisecondsRemaining / 40) + timer.IncrementMilliseconds) / 4; currentDepth++)
             Search(currentDepth, 0, -1000000000, 1000000000);
 
         return bestMove;
-
-        //        for (int currentDepth = 1; ; currentDepth++)
-        //        {
-        //            if (timer.MillisecondsElapsedThisTurn > targetMillis)
-        //                return _bestMove;
-        //            //_moveScoresDict.Clear();
-        //#if DEBUG
-        //            bestEval =
-        //#endif
-        //            Search(currentDepth, 0, -1000000000, 1000000000);
-        //#if DEBUG
-        //            ConsoleHelper.Log($"Max ply was {_maxPly} with depth {currentDepth}", false);
-        //            ConsoleHelper.Log($"Best move {_bestMove} with eval: {bestEval}. Evaluated {_evalCount} positions at depth {currentDepth} in {timer.MillisecondsElapsedThisTurn} milliseconds.", false);
-        //            _maxPly = 0;
-        //#endif
-        //        }
-
-#if DEBUG
-        //ConsoleHelper.Log($"Transposition hits: {_tableHitCount}, table has {_transpositionTable.Count} / {_transpositionTable.Capacity} entries", false, ConsoleColor.DarkRed);
-        //List<KeyValuePair<Move, int>> kvps = _moveScoresDict.ToList();
-        //kvps.Sort((a, b) => b.Value.CompareTo(a.Value));
-        //foreach (var kvp in kvps)
-        //{
-        //    ConsoleHelper.Log($"{kvp.Key} was scored {kvp.Value}.");
-        //}
-
-        //var whiteQueen = _board.GetPieceList(PieceType.Queen, true)[0];
-        //var whiteQueenBitboard = BitboardHelper.GetPieceAttacks(PieceType.Queen, whiteQueen.Square, _board, true);
-        ////var whiteQueenBitboard = BitboardHelper.GetPieceAttacks(PieceType.Queen, whiteQueen.Square, _board, true);
-        //BitboardHelper.VisualizeBitboard(whiteQueenBitboard);
-#endif
-        //return _bestMove;
 
         int Search(int depth, int ply, int alpha, int beta)
         {
@@ -155,17 +84,6 @@ public class MyBot : IChessBot
             if (board.IsDraw())
                 return 0;
 
-            bool isRoot = ply == 0;
-
-            //        if (!isRoot && _transpositionTable.TryGetEvaluation(_board.ZobristKey, depth, alpha, beta, out int tableEval))
-            //#if DEBUG
-            //        {
-            //            _tableHitCount++;
-            //#endif
-            //            return tableEval;
-            //#if DEBUG
-            //        }
-            //#endif
             // Try get evaluation from Transposition Table
             TTEntry entry = _ttEntries[board.ZobristKey % 32000];
             int tableEval = entry._evalValue;
@@ -178,8 +96,6 @@ public class MyBot : IChessBot
             int extend = 0;
             if (board.IsInCheck()) // Check extension
                 extend = 1;
-            //if (ShouldCancel)
-            //    return 0;
 
             if (depth == 0)
                 return QSearch(alpha, beta);
@@ -189,7 +105,7 @@ public class MyBot : IChessBot
             board.GetLegalMovesNonAlloc(ref legalMoves);
 
             if (legalMoves.Length > 1)
-                OrderMoves(ref legalMoves, isRoot && depth > 1);
+                OrderMoves(ref legalMoves, ply == 0 && depth > 1);
             else
                 extend = 1; // Forced move/One reply extension
             foreach (Move move in legalMoves)
@@ -200,7 +116,6 @@ public class MyBot : IChessBot
 
                 if (eval >= beta)
                 {
-                    //_transpositionTable.StoreEvaluation(depth, eval, _board.ZobristKey, TableEvalType.Beta);
                     StoreEvalInTT(eval, 2); // Beta
                     return beta;
                 }
@@ -208,12 +123,11 @@ public class MyBot : IChessBot
                 {
                     evalType = 0; // Exact
                     alpha = eval;
-                    if (isRoot)
+                    if (ply == 0)
                         bestMove = move;
                 }
             }
 
-            //_transpositionTable.StoreEvaluation(depth, alpha, _board.ZobristKey, evalType);
             StoreEvalInTT(alpha, evalType);
             return alpha;
 
@@ -281,7 +195,8 @@ public class MyBot : IChessBot
             void EvaluatePieces(PieceType pieceType, bool isWhite, int sign)
             {
                 var pieceList = board.GetPieceList(pieceType, isWhite);
-                evaluation += pieceList.Count * _pieceValues[(int)pieceType] * sign;
+                evaluation += pieceList.Count * _pieceValues[(int)pieceType] * sign; // Evaluate material value
+                // Evaluate attacks
                 foreach (var piece in pieceList)
                 {
                     var pieceBitboard = BitboardHelper.GetPieceAttacks(pieceType, piece.Square, board, isWhite);
@@ -289,6 +204,7 @@ public class MyBot : IChessBot
                     evaluation += attacks * bonusPointsPerAttack[(int)pieceType] * sign;
                 }
 
+                // Evaluate Pawns
                 if (pieceType == PieceType.Pawn)
                 {
                     var pawnFileFlags = 0;
@@ -321,7 +237,7 @@ public class MyBot : IChessBot
                             passedPawnMask |= passedPawnMask >> 16;
                             passedPawnMask |= passedPawnMask >> 32;
                         }
-                        // Passed pawn bonus, the closer to promotion the bigger
+                        // Passed pawn bonus, the closer to promotion the better
                         if ((passedPawnMask & board.GetPieceBitboard(PieceType.Pawn, !isWhite)) == 0) // Check interesction between mask and enemy pawns
                             evaluation += 20 * (isWhite ? pawn.Square.Rank : 7 - pawn.Square.Rank) * sign;
                     }
@@ -341,7 +257,6 @@ public class MyBot : IChessBot
         {
             for (int i = 0; i < moves.Length; i++)
             {
-                //_moveScores[i] = GuessMoveScore(moves[i]);
                 Move move = moves[i];
                 _moveScores[i] = 0;
                 if (useBestMove && move == bestMove)
@@ -361,70 +276,8 @@ public class MyBot : IChessBot
             }
 
             _moveScores.AsSpan().Slice(0, moves.Length).Sort(moves, (a, b) => b.CompareTo(a));
-
-            //int GuessMoveScore(Move move)
-            //{
-            //    if (useBestMove && move == _bestMove)
-            //        return 10000000;
-            //    int guess = 0;
-            //    var movedPiece = move.MovePieceType;
-            //    var capturedPiece = move.CapturePieceType;
-
-            //    if (capturedPiece != PieceType.None)
-            //        guess = 10 * _pieceValues[(int)capturedPiece] - _pieceValues[(int)movedPiece];
-
-            //    if (move.IsPromotion)
-            //        guess += _pieceValues[(int)move.PromotionPieceType];
-
-            //    if (move.IsCastles)
-            //        guess += 10000;
-
-            //    return guess;
-            //}
         }
     }
-
-
-    //public class TranspositionTable
-    //{
-    //    private ulong _capacity;
-    //    private TTEntry[] _entries;
-
-    //    private ulong GetIndex(ulong zobristkey) => zobristkey % _capacity;
-
-    //    public TranspositionTable(ulong capacity)
-    //    {
-    //        _capacity = capacity;
-    //        _entries = new TTEntry[capacity];
-    //    }
-
-    //    public void StoreEvaluation(int depth, int eval, ulong zobristKey, TableEvalType nodeType)
-    //    {
-    //        _entries[GetIndex(zobristKey)] = new TTEntry(eval, (byte)depth, nodeType, zobristKey);
-    //    }
-
-    //    public bool TryGetEvaluation(ulong zobristKey, int depth, int alpha, int beta, out int eval)
-    //    {
-    //        TTEntry entry = _entries[GetIndex(zobristKey)];
-    //        if (entry._zobristKey == zobristKey)
-    //        {
-    //            if (entry._depth >= depth)
-    //            {
-    //                eval = entry._evalValue;
-    //                if (entry._nodeType == TableEvalType.Exact)
-    //                    return true;
-    //                if (entry._nodeType == TableEvalType.Alpha && eval <= alpha)
-    //                    return true;
-    //                if (entry._nodeType == TableEvalType.Beta && eval >= beta)
-    //                    return true;
-    //            }
-    //        }
-    //        eval = 0;
-    //        return false;
-    //    }
-
-
-    //}
 
     public struct TTEntry
     {
@@ -433,14 +286,6 @@ public class MyBot : IChessBot
         public byte _nodeType; // 0 = Exact, 1 = Alpha, 2 = Beta
         public ulong _zobristKey;
 
-        //public TTEntry(int eval, byte depth, TableEvalType nodeType, ulong zobristKey)
-        //{
-        //    _evalValue = eval;
-        //    _depth = depth;
-        //    _nodeType = nodeType;
-        //    _zobristKey = zobristKey;
-        //}
-
 #if DEBUG
         public static int GetSize()
         {
@@ -448,11 +293,4 @@ public class MyBot : IChessBot
         }
 #endif
     }
-
-    //public enum TableEvalType : byte
-    //{
-    //    Exact,
-    //    Alpha,
-    //    Beta
-    //}
 }
